4a1f914b2cd5b975ecfa17dba0f9dc27bb44aa8f
Ro1dex <sanhcez58@gmail.com>
2023-02-10 21:15:55 +0200

    com

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,28 +1,15 @@
 package ru.netology;
 
+import ru.netology.sqrtcalcul.sqr.SqrtService;
 
 public class Main {
     public static void main(String[] args) {
 
-        int lowerValue = 10;
-        int upperValue = 99;
-        int i;
-    //    int count = 0;
-        //мне нужно просто найти количество итераций цикла
-        // А КАК???
+        int lowerValue = 26;
+        int upperValue = 36;
 
-
-        for (i = 1; (i * i) <= upperValue; i++) {
-            if ((i * i) <= lowerValue) {
-                    continue;
-            }
-            System.out.println("Число квадратов в заданном диапазоне:" + i);
-
-        }
-
-
-
-
+        SqrtService service = new SqrtService();
+        System.out.println(service.calcCount(lowerValue, upperValue));
 
     }
 
